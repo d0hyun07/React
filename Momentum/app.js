186a3ed3eb7232a7ed3af1797009ae32dcc3f7c6
KDH <strongdh07@gmail.com>
2023-09-09 21:30:05 +0900

    2023-09-09

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,3 +1,23 @@
 // alert('hi');
 
-console.log("lalalalala"); 
+// console.log("lalalalala"); 
+// const amIFat = true;
+// console.log(amIFat);
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sund";
+
+const daysOfWeek = [mon,tue,wed,thu,fri,sat,sun];
+
+const nonsense = [1,2,"hello", false, null, true, undefined,"nico"];
+
+console.log(daysOfWeek);
+console.log(nonsense);
+
+daysOfWeek.push("holiday");
+console.log(daysOfWeek);
