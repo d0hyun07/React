6f3ba0efe914fa3787bc39b20fabe43daa1ed92d
KDH <strongdh07@gmail.com>
2023-10-04 02:22:15 +0900

    2023-10-04

--- middleClassJS/src/Array.js
+++ middleClassJS/src/Array.js
@@ -8,17 +8,60 @@
 
 // find / findIndex
 
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
-const result = arr.find((item) => {
-  return item%2==0;
-});
+// const result = arr.find((item) => {
+//   return item%2==0;
+// });
 
-console.log(result)
+// console.log(result)
 
-let userList = [
-  {name: "Mike", age: 30},
-  {name: "Jane", age: 27},
-  {name: "Tom", age: 310},
+// let userList = [
+//   {name: "Mike", age: 9},
+//   {name: "Jane", age: 27},
+//   {name: "Tom", age: 10},
+// ];
 
-]
+// const result = userList.find((user) => {
+//   if(user.age < 10){
+//     return true;
+//   }
+//   return false;
+// });
+
+// console.log(result);
+
+// const result2 = userList.filter((item) => {
+//   return item%2==0;
+// });
+
+// let newUserList = userList.map((user, index) => {
+//   return Object.assign({}, user, {
+//     isAdult: user.age > 19,
+//   });
+// });
+
+// console.log(result);
+// console.log(result2);
+// console.log(newUserList);
+
+// join, split
+
+let arr = ["안녕", "나는", "철수야"];
+
+let result = arr.join(" ");
+
+console.log(result);
+
+let user = {
+  name: "Mike",
+  age: 30,
+};
+
+let userList = ["Mike", "Tom", "Jane"];
+
+console.log(typeof user);
+console.log(typeof userList);
+
+console.log(Array.isArray(user));
+console.log(Array.isArray(userList));
