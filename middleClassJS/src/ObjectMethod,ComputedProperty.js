11dac4bf22607a8212ea4abd47e556c54463e93f
KDH <strongdh07@gmail.com>
2023-10-03 22:18:13 +0900

    2023-10-03

---
+++ middleClassJS/src/ObjectMethod,ComputedProperty.js
@@ -0,0 +1,43 @@
+// let n = "name";
+// let a = "age";
+
+// const user = {
+//   [n] : "Mike",
+//   [a] : 30,
+// };
+
+// function makeObj(key, val) {
+//   return {
+//     [key] : val
+//   }
+// }
+
+// const obj = makeObj("이름",33);
+
+// console.log(obj);
+
+// const user = {
+//   name : "Mike",
+//   age : 30,
+// };
+
+// const user2 = Object.assign({},user);
+
+// console.log(user2);
+
+// const result = Object.keys(user);
+// const result2 = Object.entries(user);
+// // const result3 = Object.fromEntries(user);
+
+// console.log(result);
+// console.log(result2);
+// // console.log(result3);
+
+// let arr = [
+//   ['mon', '월'],
+//   ['tue', '화']
+// ];
+
+// const result4 = Object.fromEntries(arr);
+
+
