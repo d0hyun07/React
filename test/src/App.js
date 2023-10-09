f97cb05ecd42f153314a1dfdcd465141e2cadb9f
KDH <strongdh07@gmail.com>
2023-10-09 20:12:16 +0900

    2023-10-09

--- test/src/App.js
+++ test/src/App.js
@@ -1,18 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Hello from './component/Hello';
-import Welcome from './component/Welcome'
+import Welcome from './component/Welcome';
+import styles from './App.module.css';
 
 function App() {
-  const name = "Tom";
-  const naver = {
-    name: '네이버',
-    url: "https://naver.com",
-  }
   return (
     <div className="App">
-      <Hello></Hello>
-      <Welcome></Welcome>
+      <Hello />
+      <div className={styles.box}></div>
     </div>
   );
 }
--- test/src/component/Hello.js
+++ test/src/component/Hello.js
@@ -1,5 +1,28 @@
-
+import Welcome from './Welcome';
 
 export default function Hello() {
-  return <h1>Hello</h1>
+  function showName() {
+    console.log("Mike");
+  }
+  function showAge(age) {
+    console.log(age);
+  }
+  function showText(e) {
+    console.log();
+  }
+  return (
+    <div>
+      <h1>Hello</h1>
+      <button onClick={showName()}>Show name</button>
+      <button 
+      onClick={() => {
+          showAge(30);
+        }
+      }>
+        Show age
+      </button>
+      <Welcome />
+      <input type="text" onChange={showText}/>
+    </div>
+  );
 };
